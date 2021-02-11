8f71513e1d2f0af1c6b32041b1b38f4bb5f356c3
Juan Castro <info@jempe.org>
2021-02-11 08:13:33 -0500

    resize badge base

--- cmd/lmbadge/main.go
+++ cmd/lmbadge/main.go
@@ -9,17 +9,45 @@ import (
 	"os"
 
 	"github.com/golang/freetype"
+	"github.com/nfnt/resize"
 	"golang.org/x/image/font"
 )
 
 func main() {
-	fmt.Println("vim-go")
+	badgeSize := 500
 
-	width := 100
-	height := 100
+	var badgeNoText image.Image
+
+	file, err := os.Open("laughing_man.png")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+
+	}
+
+	badgeNoText, err = png.Decode(file)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+
+	}
+
+	file.Close()
+
+	resizedBadge := resize.Thumbnail(uint(badgeSize), uint(badgeSize), badgeNoText, resize.Lanczos3)
+
+	im := image.NewRGBA(image.Rectangle{Max: image.Point{X: badgeSize, Y: badgeSize}})
+
+	for x := 0; x < badgeSize; x++ {
+
+		for y := 0; y < badgeSize; y++ {
+			r, g, b, a := resizedBadge.At(x, y).RGBA()
+			im.SetRGBA(x, y, color.RGBA{R: uint8(r), G: uint8(g), B: uint8(b), A: uint8(a)})
+		}
+	}
 
 	// Read the font data.
-	fontBytes, err := ioutil.ReadFile("/usr/share/fonts/truetype/freefont/FreeMono.ttf")
+	fontBytes, err := ioutil.ReadFile("./fonts/AWAKE.ttf")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -30,37 +58,25 @@ func main() {
 		os.Exit(1)
 	}
 
-	im := image.NewRGBA(image.Rectangle{Max: image.Point{X: width, Y: height}})
-
-	for x := 0; x < width; x++ {
-
-		for y := 0; y < height; y++ {
-
-			im.SetRGBA(x, y, color.RGBA{R: 255, G: 255, B: 255, A: 255})
-
-		}
-
-	}
-
-	fg := image.Black
+	fg := image.NewUniform(color.NRGBA{255, 128, 128, 255})
 
 	c := freetype.NewContext()
 	c.SetDPI(72)
 	c.SetFont(f)
-	c.SetFontSize(15)
+	c.SetFontSize(30.5)
 	c.SetClip(im.Bounds())
 	c.SetDst(im)
 	c.SetSrc(fg)
 	c.SetHinting(font.HintingNone)
 
-	pt := freetype.Pt(10, 10+int(c.PointToFixed(15)>>6))
-	_, err = c.DrawString("this is a test", pt)
+	pt := freetype.Pt(10, 10+int(c.PointToFixed(30.5)>>6))
+	_, err = c.DrawString("KOTOKO", pt)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	out, err := os.Create("/home/kastro/Desktop/lm.png")
+	out, err := os.Create("lm.png")
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
